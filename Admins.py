d9080949d4bf3878b25a19ec55429210f5e27783
potionsnotusedlol <mskomissarov07@gmail.com>
2026-06-02 09:57:24 +0300

    acceptable notifications fetching/interface building

--- Admins.py
+++ Admins.py
@@ -1,12 +1,14 @@
 from aiogram import Router, Bot
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, StateFilter
 from aiogram.types import Message, CallbackQuery, InputMediaPhoto, FSInputFile
 from aiogram.utils.formatting import Text
 from aiogram.methods.send_message import SendMessage
 from aiogram import F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from Middlewares import RoleFilter, Role
 from StudentDataHandler import SDH, Request
-from KB import ADMIN_MAIN_MENU
+from KB import ADMIN_MAIN_MENU, RETURN_HOME, fetchNotifications, buildNotificationsKeyboard, initNotificationsFile
 from Config import config
 
 import json
@@ -46,36 +48,56 @@ _В случае ошибки, файл не будет принят._
 router = Router()
 router.message.filter(RoleFilter(Role.ADMIN, Role.OWNER))
 STORAGE_DIR=config.STORAGE_FOLDER.get_secret_value()
-notifications = []
 
 @router.message(CommandStart)
 async def greet(msg: Message):
     await msg.answer(**Text(GREETING).as_kwargs())
     await msg.answer(MAIN_MENU, reply_markup=ADMIN_MAIN_MENU)
+    await initNotificationsFile()
+
+@router.callback_query(F.data == "menus")
+async def returnHome(callback: CallbackQuery, bot: Bot):
+    await callback.answer()
+    await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
+    await bot.send_message(chat_id=callback.message.chat.id, text=MAIN_MENU, reply_markup=ADMIN_MAIN_MENU)
 
 @router.callback_query(F.data.startswith("update"))
 async def getUpdatedData(callback: CallbackQuery, bot: Bot):
     function = callback.data.split(" ")[1] # type: ignore
 
     if function == "users":
-        await callback.answer()
+        await callback.answer(reply_markup=RETURN_HOME)
 
-        hint_pic = InputMediaPhoto(media=FSInputFile("message_media/users_table_struct.jpeg"), caption=SEND_ROLES_HINT)
+        hint_pic = InputMediaPhoto(media=FSInputFile("message_media/users_table_struct.jpeg"))
 
-        await bot.edit_message_media(chat_id=callback.message.chat.id, message_id=callback.message.message_id, media=hint_pic) # type: ignore
+        await bot.edit_message_media(chat_id=callback.message.chat.id, message_id=callback.message.message_id, media=hint_pic, reply_markup=RETURN_HOME) # type: ignore
+        await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id, text=SEND_ROLES_HINT)
         pass
     elif function == "DB":
         await callback.answer()
-        await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id, text=SEND_INFO_HINT) # type: ignore
+        await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id, text=SEND_INFO_HINT, reply_markup=RETURN_HOME) # type: ignore
         pass
 
-async def fetchNotifications(username: str) -> dict:
-    with open(STORAGE_DIR + "notifications.json", "r", encoding="utf-8") as F:
-        total = json.load(F)
+class SetTime(StatesGroup):
+    entering_time = State()
 
 @router.callback_query(F.data == "notifications setup")
-async def setupNotifications(callback: CallbackQuery):
-    pass
+async def setupNotifications(callback: CallbackQuery, bot: Bot):
+    await callback.answer()
+    SETUP_NOTIFICATIONS_MENU = await buildNotificationsKeyboard(callback.from_user.username)
+    await bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id, text="Выберите день для настройки сообщений", reply_markup=SETUP_NOTIFICATIONS_MENU.as_markup())
+
+@router.callback_query(F.data.startswith("selected"))
+async def selectDay(callback: CallbackQuery):
+    day = callback.data.split(" ")[1] # type: ignore
+    notifications = await fetchNotifications(callback.from_user.username, day) # type: ignore
+    answer_text = "Напишите время напоминания в формате *ЧЧ:ММ* для добавления/удаления напоминания\n"
+
+    for notification in notifications:
+        answer_text += (notification + "\n")
+
+    callback.answer(answer_text, reply_markup=RETURN_HOME)
+
 
 """{
     "users": [
--- KB.py
+++ KB.py
@@ -1,6 +1,11 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from Config import config
 
+import json
+import os
+
+STORAGE_DIR=config.STORAGE_FOLDER.get_secret_value()
 ADMIN_MAIN_MENU_LAYOUT = [
     [
         InlineKeyboardButton(text="🥤 Обновить информацию по пользователям", callback_data="update users"),
@@ -9,13 +14,75 @@ ADMIN_MAIN_MENU_LAYOUT = [
     [InlineKeyboardButton(text="🂁 Настроить напоминания", callback_data="notifications setup")]
 ]
 ADMIN_MAIN_MENU = InlineKeyboardMarkup(inline_keyboard=ADMIN_MAIN_MENU_LAYOUT)
-SETUP_NOTIFICATIONS_MENU_LAYOUT = [
-    [InlineKeyboardButton(text="Понедельник ❌", callback_data="selected monday")],
-    [InlineKeyboardButton(text="Вторник ❌", callback_data="selected tuesday")],
-    [InlineKeyboardButton(text="Среда ❌", callback_data="selected wendesday")],
-    [InlineKeyboardButton(text="Четверг ❌", callback_data="selected thursday")],
-    [InlineKeyboardButton(text="Пятница ✅", callback_data="selected friday")],
-    [InlineKeyboardButton(text="Суббота ❌", callback_data="selected saturday")],
-    [InlineKeyboardButton(text="Воскресенье ❌", callback_data="selected sunday")]
-]
-SETUP_NOTIFICATIONS_MENU = InlineKeyboardMarkup(inline_keyboard=SETUP_NOTIFICATIONS_MENU_LAYOUT)
+RETURN_HOME_LAYOUT = [[InlineKeyboardButton(text="В главное меню🚀", callback_data="menus")]]
+RETURN_HOME = InlineKeyboardMarkup(inline_keyboard=RETURN_HOME_LAYOUT)
+# SETUP_NOTIFICATIONS_MENU = InlineKeyboardBuilder()
+WEEKDAYS = {
+    "monday": "Понедельник ",
+    "tuesday": "Вторник ",
+    "wendesday": "Среда ",
+    "thursday": "Четверг ",
+    "friday": "Пятница ",
+    "saturday": "Суббота ",
+    "sunday": "Воскресенье "
+}
+
+async def initNotificationsFile():
+    file_path = STORAGE_DIR + "notifications.json"
+
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as F:
+            total = {"users": []}
+
+            json.dump(total, F, indent=4)
+
+async def initUserNotifications(username: str):
+    with open(STORAGE_DIR + "notifications.json", "r") as F:
+        content = json.load(F)
+        new_data = {
+            "username": username,
+            "times": []
+        }
+
+        content["users"].append(new_data)
+    
+    with open(STORAGE_DIR + "notifications.json", "w") as F:
+        json.dump(content, F, indent=4)
+
+async def fetchNotifications(username: str, weekday: str) -> list:
+    with open(STORAGE_DIR + "notifications.json", "r") as F:
+        total = json.load(F)
+
+        for i in total["users"]:
+            if i["username"] == username:
+                weekday_times = [
+                    time_dict[weekday]
+                    for time_dict in i["times"]
+                    if weekday in time_dict
+                ]
+                
+                return weekday_times
+        
+    await initUserNotifications(username)
+
+    return []
+
+async def buildNotificationsKeyboard(username: str) -> InlineKeyboardBuilder:
+    SETUP_NOTIFICATIONS_MENU = InlineKeyboardBuilder()
+
+    for eng, rus in WEEKDAYS.items():
+        cd = "selected " + eng
+        check = await fetchNotifications(username, eng)
+
+        if check == []:
+            txt = rus + "❌"
+
+            SETUP_NOTIFICATIONS_MENU.row(InlineKeyboardButton(text=txt, callback_data=cd))
+        else:
+            txt = rus + "✅"
+
+            SETUP_NOTIFICATIONS_MENU.row(InlineKeyboardButton(text=txt, callback_data=cd))
+        
+    SETUP_NOTIFICATIONS_MENU.row(InlineKeyboardButton(text="Вернуться в главное меню 🎽", callback_data="menus"))
+
+    return SETUP_NOTIFICATIONS_MENU
